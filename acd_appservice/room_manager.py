0a41c86474338e8f18c2d8f390d5c388b8b77a7d
Alejandro Herrera <bramendev@gmail.com>
2022-04-25 11:19:02 -0500

    WIP

--- acd_appservice/room_manager.py
+++ acd_appservice/room_manager.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import asyncio
 import logging
 import re
+from typing import Dict
 
 from mautrix.api import Method
 from mautrix.appservice import IntentAPI
@@ -11,12 +12,11 @@ from mautrix.util.logging import TraceLogger
 
 from .config import Config
 
-# from acd_program.puppet import Puppet
-
 
 class RoomManager:
     config: Config
     log: TraceLogger = logging.getLogger("mau.room_manager")
+    ROOMS: dict[RoomID, Dict] = {}
 
     def __init__(self, config: Config) -> None:
         self.config = config
@@ -115,7 +115,7 @@ class RoomManager:
 
             customer_displayname = await intent.get_displayname(sender)
             if customer_displayname:
-                room_name = f"{customer_displayname} ({phone_match[0]})"
+                room_name = f"{customer_displayname}({phone_match[0]})"
             else:
                 room_name = f"({phone_match[0]})"
 
@@ -161,57 +161,93 @@ class RoomManager:
     # OTRO TIPO DE SALA (Cuando es la sala de control, sala de agentes o de colas)
 
     async def is_customer_room(self, room_id: RoomID, intent: IntentAPI) -> bool:
-        creator = await RoomManager.get_room_creator(room_id=room_id, intent=intent)
+
+        creator = await self.get_room_creator(room_id=room_id, intent=intent)
+
+        try:
+            room = self.ROOMS[room_id]
+            is_customer_room = room.get("is_customer_room")
+            if is_customer_room:
+                return is_customer_room
+        except KeyError:
+            pass
+
         bridges = self.config["bridges"]
         if creator:
             for bridge in bridges:
                 user_prefix = self.config[f"bridges.{bridge}.user_prefix"]
                 if creator.startswith(f"@{user_prefix}"):
+                    self.ROOMS[room_id]["is_customer_room"] = True
                     return True
         return False
 
-    async def is_mx_whatsapp_status_broadcast(self, room_id: RoomID, intent: IntentAPI) -> bool:
+    async def get_room_creator(self, room_id: RoomID, intent: IntentAPI) -> str:
+        creator = None
+
         try:
-            room_name = await RoomManager.get_room_name(room_id=room_id, intent=intent)
+            room = self.ROOMS[room_id]
+            return room.get("creator")
+        except KeyError:
+            pass
+
+        try:
+            room_info = await self.get_room_info(room_id=room_id, intent=intent)
+            creator = room_info.get("creator")
         except Exception as e:
             self.log.error(e)
 
-        if room_name == "WhatsApp Status Broadcast":
+        return creator
+
+    async def is_mx_whatsapp_status_broadcast(self, room_id: RoomID, intent: IntentAPI) -> bool:
+        room_name = None
+        try:
+            room_name = await self.get_room_name(room_id=room_id, intent=intent)
+        except Exception as e:
+            self.log.error(e)
+
+        if room_name and room_name == "WhatsApp Status Broadcast":
             return True
 
         return False
 
     async def get_room_bridge(self, room_id: RoomID, intent: IntentAPI) -> str:
+        try:
+            room = self.ROOMS[room_id]
+            bridge = room.get("bridge")
+            if bridge:
+                return bridge
+        except KeyError:
+            pass
+
         creator = await RoomManager.get_room_creator(room_id=room_id, intent=intent)
+
         bridges = self.config["bridges"]
         if creator:
             for bridge in bridges:
                 user_prefix = self.config[f"bridges.{bridge}.user_prefix"]
                 if creator.startswith(f"@{user_prefix}"):
                     self.log.debug(f"The bridge obtained is {bridge}")
+                    self.ROOMS[room_id]["brdige"] = bridge
                     return bridge
         return None
 
-    @classmethod
-    async def get_room_creator(cls, room_id: RoomID, intent: IntentAPI) -> str:
+    async def get_room_name(self, room_id: RoomID, intent: IntentAPI) -> str:
+        room_name = None
         try:
-            room_info = await intent.api.request(
-                method=Method.GET, path=f"/_synapse/admin/v1/rooms/{room_id}"
-            )
-            creator = room_info.get("creator")
-        except Exception as e:
-            cls.log.error(e)
-
-        return creator
-
-    @classmethod
-    async def get_room_name(cls, room_id: RoomID, intent: IntentAPI) -> str:
-        try:
-            room_info = await intent.api.request(
-                method=Method.GET, path=f"/_synapse/admin/v1/rooms/{room_id}"
-            )
+            room_info = await self.get_room_info(room_id=room_id, intent=intent)
             room_name = room_info.get("name")
         except Exception as e:
-            cls.log.error(e)
+            self.log.error(e)
 
         return room_name
+
+    async def get_room_info(self, room_id: RoomID, intent: IntentAPI) -> Dict:
+        try:
+            room_info = await intent.api.request(
+                method=Method.GET, path=f"/_synapse/admin/v1/rooms/{room_id}"
+            )
+            self.ROOMS[room_id] = room_info
+        except Exception as e:
+            self.log.error(e)
+
+        return room_info
