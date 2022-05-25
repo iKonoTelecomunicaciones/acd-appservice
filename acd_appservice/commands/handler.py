695cfc05c4e385b67b91b37b08605e780ee47f80
Alejandro Herrera <bramendev@gmail.com>
2022-05-25 14:27:13 -0500

    Fixed anythins

--- acd_appservice/commands/handler.py
+++ acd_appservice/commands/handler.py
@@ -83,7 +83,7 @@ async def command_processor(cmd_evt: CommandEvent):
         if result:
             return result
     else:
-        await cmd_evt.reply("Unrecognized command")
+        f"Unrecognized command - Use **`{cmd_evt.command_prefix} help`** for more information"
 
 
 def make_help_text(command_prefix: str) -> str:
