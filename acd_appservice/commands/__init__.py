860b3524df903c0b2fd098899740332a7fd81407
Alejandro Herrera <bramendev@gmail.com>
2022-06-06 15:19:52 -0500

    WIP

--- acd_appservice/commands/__init__.py
+++ acd_appservice/commands/__init__.py
@@ -1,5 +1,6 @@
 from .acd import acd
 from .pm import pm
 from .resolve import resolve
+from .state_event import state_event
 from .template import template
 from .transfer import transfer, transfer_user
