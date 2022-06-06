35cd7fe62d765785f6d5983e07ff3f984708a1d2
Alejandro Herrera <bramendev@gmail.com>
2022-06-06 15:17:34 -0500

    WIP

--- acd_appservice/commands/state_event.py
+++ acd_appservice/commands/state_event.py
@@ -1,22 +1,52 @@
-import asyncio
 import json
-import re
 from typing import Dict
 
-from aiohttp import ClientSession
-from markdown import markdown
+from mautrix.types import EventType
 
-from ..http_client import ProvisionBridge
-from ..puppet import Puppet
-from ..signaling import Signaling
 from .handler import command_handler
 from .typehint import CommandEvent
 
 
 @command_handler(
-    name="pm",
-    help_text=("Command that allows send a message to a customer"),
+    name="state_event",
+    help_text=("Command that sends a status event to matrix"),
     help_args="<_dict_>",
 )
-async def pm(evt: CommandEvent) -> Dict:
-    pass
+async def state_event(evt: CommandEvent) -> Dict:
+    if len(evt.args) <= 1:
+        detail = "state_event command incomplete arguments"
+        evt.log.error(detail)
+        evt.reply(text=detail)
+        return
+
+    prefix_and_command_length = len(f"{evt.command_prefix} {evt.cmd}")
+
+    incoming_message = (evt.text[prefix_and_command_length:]).strip()
+
+    try:
+        incoming_params = json.loads(incoming_message)
+    except Exception as e:
+        evt.log.error(f"Error processing incoming params, skipping message - {e}")
+        return
+
+    room_id = incoming_params.get("room_id")
+    event_type = incoming_params.get("event_type")
+
+    # Validating incoming params
+    if not room_id:
+        evt.log.error(f"You must specify a room ID to process_state_event")
+        return
+
+    if not event_type:
+        evt.log.error(
+            f"You must specify a type event for the room: {room_id} - process_state_event"
+        )
+        return
+
+    if event_type == "ik.chat.tag":
+        event_type = EventType.find("ik.chat.status", EventType.Class.STATE)
+        content = {"tags": incoming_params.get("tags")}
+
+    await evt.agent_manager.signaling.send_state_event(
+        room_id=room_id, event_type=event_type, content=content
+    )
