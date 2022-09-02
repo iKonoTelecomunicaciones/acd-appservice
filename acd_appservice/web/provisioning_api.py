9f753176342d081646e383e14cd1422209cb3d3a
Alejandro Herrera <bramendev@gmail.com>
2022-09-02 09:52:59 -0500

    Added doc for the endpoint create_user

--- acd_appservice/web/provisioning_api.py
+++ acd_appservice/web/provisioning_api.py
@@ -171,10 +171,10 @@ class ProvisioningAPI:
                   user_email:
                     type: string
                 example:
-                    user_email: @acd1:somewhere.com
-                    control_room_id: !foo:somewhere.com
-                    menubot_id: nobody@somewhere.com
-                    bridge: nobody@somewhere.com
+                    user_email: "@acd1:somewhere.com"
+                    control_room_id: "!foo:somewhere.com"
+                    menubot_id: "nobody@somewhere.com"
+                    bridge: "nobody@somewhere.com"
         responses:
             '201':
                 $ref: '#/components/responses/UserCreated'
