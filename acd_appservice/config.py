bccced4b9f78e8183622c60c493e01db6ad73c70
egalvis <egalvis@ikono.com.co>
2022-09-16 13:14:58 -0500

    solve config.py error in leave_or_kick param

--- acd_appservice/config.py
+++ acd_appservice/config.py
@@ -50,7 +50,7 @@ class Config(BaseBridgeConfig):
         copy_dict("acd.offline")
         copy("acd.no_agents_for_transfer")
         copy_dict("acd.resolve_chat")
-        copy["acd.leave_or_kick"]
+        copy("acd.leave_or_kick")
 
     @property
     def namespaces(self) -> dict[str, list[dict[str, Any]]]:
