bf38b6ed7166fc1d413f55d09b2998d53743df54
Alejandro Herrera <bramendev@gmail.com>
2023-02-21 16:10:59 -0500

    Added new method to check if the agent is online

--- acd_appservice/commands/transfer.py
+++ acd_appservice/commands/transfer.py
@@ -1,11 +1,9 @@
 import asyncio
 
-from mautrix.types import PresenceState
-
 from ..portal import Portal
 from ..puppet import Puppet
 from ..queue import Queue
-from ..queue_membership import QueueMembershipState
+from ..user import User
 from .handler import CommandArg, CommandEvent, command_handler
 
 agent_id = CommandArg(
@@ -73,6 +71,7 @@ async def transfer(evt: CommandEvent) -> str:
         return {"data": {"error": detail}, "status": 422}
 
     puppet: Puppet = await Puppet.get_customer_room_puppet(room_id=customer_room_id)
+    portal: Portal = await Portal.get_by_room_id(room_id=customer_room_id, fk_puppet=puppet.pk)
 
     if not puppet:
         return
@@ -86,24 +85,23 @@ async def transfer(evt: CommandEvent) -> str:
 
     # Locking the room so that no other transfer can be made to the room.
     puppet.room_manager.lock_room(room_id=customer_room_id, transfer=True)
-    is_agent = puppet.agent_manager.is_agent(agent_id=evt.sender.mxid)
 
     # Checking if the sender is an agent, if not, it gets the agent id from the room.
-    if is_agent:
-        transfer_author = evt.sender.mxid
+    if evt.sender.is_agent:
+        transfer_author = evt.sender
     else:
-        agent_id = await puppet.agent_manager.get_room_agent(room_id=customer_room_id)
-        transfer_author = agent_id
+        agent = await portal.get_current_agent()
+        transfer_author = agent
 
-    portal: Portal = await Portal.get_by_room_id(room_id=customer_room_id, fk_puppet=puppet.pk)
     queue: Queue = await Queue.get_by_room_id(room_id=campaign_room_id)
+
     # Creating a task that will be executed in the background.
     asyncio.create_task(
         puppet.agent_manager.loop_agents(
             portal=portal,
             queue=queue,
             agent_id=puppet.agent_manager.CURRENT_AGENT.get(campaign_room_id),
-            transfer_author=transfer_author or evt.sender.mxid,
+            transfer_author=transfer_author,
         )
     )
 
@@ -144,6 +142,7 @@ async def transfer_user(evt: CommandEvent) -> str:
 
     puppet: Puppet = await Puppet.get_customer_room_puppet(room_id=customer_room_id)
     portal: Portal = await Portal.get_by_room_id(room_id=customer_room_id, fk_puppet=puppet.pk)
+    agent: User = await User.get_by_mxid(agent_id)
 
     try:
         force = evt.args_list[2]
@@ -158,53 +157,32 @@ async def transfer_user(evt: CommandEvent) -> str:
         evt.log.debug(f"Room: {portal.room_id} LOCKED by Transfer user")
         return
 
-    evt.log.debug(f"INIT TRANSFER for {portal.room_id} to AGENT {agent_id}")
+    evt.log.debug(f"INIT TRANSFER for {portal.room_id} to AGENT {agent.mxid}")
 
     # Locking the room so that no other transfer can be made to the room.
     puppet.room_manager.lock_room(room_id=portal.room_id, transfer=True)
-    is_agent = puppet.agent_manager.is_agent(agent_id=evt.sender.mxid)
 
     # Checking if the sender is an agent, if not, it gets the agent id from the room.
-    if is_agent:
+    if evt.sender.is_agent:
         transfer_author = evt.sender.mxid
     else:
-        _agent_id = await puppet.agent_manager.get_room_agent(room_id=portal.room_id)
-        transfer_author = _agent_id
+        _agent = await portal.get_current_agent()
+        transfer_author = _agent.mxid
 
     # Checking if the agent is already in the room, if so, it sends a message to the room.
-    if transfer_author == agent_id:
-        msg = f"The {agent_id} agent is already in the room {portal.room_id}"
+    if transfer_author == agent.mxid:
+        msg = f"The {agent.mxid} agent is already in the room {portal.room_id}"
         await evt.intent.send_notice(room_id=portal.room_id, text=msg)
     else:
-        # Switch between presence and agent operation login using config parameter
-        # to verify if agent is available to be assigned to the chat
-        if evt.config["acd.use_presence"]:
-            presence_response = await puppet.agent_manager.get_agent_presence(agent_id=agent_id)
-            is_agent_online = (
-                presence_response and presence_response.presence == PresenceState.ONLINE
-            )
-        else:
-            presence_response = await puppet.agent_manager.get_agent_status(agent_id=agent_id)
-            is_agent_online = (
-                presence_response
-                and presence_response.get("presence") == QueueMembershipState.Online.value
-            )
 
-        evt.log.debug(
-            f"PRESENCE RESPONSE: "
-            f"[{agent_id}] -> "
-            f"""[{presence_response.get('presence') or presence_response.presence
-            if presence_response else None}]"""
-        )
-
-        if is_agent_online or force == "yes":
+        if await agent.is_online() or force == "yes":
             await puppet.agent_manager.force_invite_agent(
                 portal=portal,
-                agent_id=agent_id,
+                agent_id=agent.mxid,
                 transfer_author=transfer_author or evt.sender.mxid,
             )
         else:
-            msg = f"Agent {agent_id} is not available"
+            msg = f"Agent {agent.mxid} is not available"
             await evt.intent.send_notice(room_id=portal.room_id, text=msg)
 
     puppet.room_manager.unlock_room(room_id=portal.room_id, transfer=True)
