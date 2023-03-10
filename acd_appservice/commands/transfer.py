69a5d4c7d5f5e6e0b99803763848f1b4c279e015
egalvis <egalvis@ikono.com.co>
2023-03-10 08:49:00 -0500

    Corrected config parameter

--- acd_appservice/commands/transfer.py
+++ acd_appservice/commands/transfer.py
@@ -224,8 +224,8 @@ async def transfer_user(evt: CommandEvent) -> str:
                         "but they are not available to attend the chat."
                     )
                     json_response["status"] = 202
-                    if evt.config["acd.unavailabe_agent_in_transfer"]:
-                        msg = evt.config["acd.unavailabe_agent_in_transfer"].format(
+                    if evt.config["acd.unavailable_agent_in_transfer"]:
+                        msg = evt.config["acd.unavailable_agent_in_transfer"].format(
                             agentname=agent_displayname
                         )
                         await portal.send_formatted_message(msg)
