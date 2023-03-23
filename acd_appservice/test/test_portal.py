ab1d13af5a2bd31d39d8bc6f194ba4676b301d9e
egalvis <egalvis@ikono.com.co>
2023-03-23 17:27:50 -0500

    Improved test documentation

--- acd_appservice/test/test_portal.py
+++ acd_appservice/test/test_portal.py
@@ -17,32 +17,41 @@ nest_asyncio.apply()
 @pytest.mark.asyncio
 class TestPortal:
     async def test_get_by_room_id(self, acd_init):
+        """Gets a portal object given the room_id"""
+
         # Create a puppet in database
         query = "INSERT INTO puppet (custom_mxid) values ($1) RETURNING pk"
         await Portal.db.execute(query, "@acd1:example.com")
 
-        room_id: RoomID = "!qVKwlyUXOCrBfZJOdh:exmple.com"
+        room_id: RoomID = "!qVKwlyUXOCrBfZJOdh:example.com"
         await Portal.get_by_room_id(room_id=room_id, fk_puppet=1)
         portal: Portal = await Portal.get_by_room_id(room_id=room_id, create=False)
 
         assert portal.room_id == room_id
 
     async def test_get_by_room_id_create(self, acd_init):
+        """
+        It tries to get a portal to the database,
+        if it doesn't exist, it creates a portal in database
+        """
+
         # Create a puppet in database
         query = "INSERT INTO puppet (custom_mxid) values ($1) RETURNING pk"
         await Portal.db.execute(query, "@acd1:example.com")
 
-        room_id: RoomID = "!qVKwlyUXOCrBfZJOdh:exmple.com"
+        room_id: RoomID = "!qVKwlyUXOCrBfZJOdh:example.com"
         portal: Portal = await Portal.get_by_room_id(room_id=room_id, fk_puppet=1)
 
         assert portal.room_id == room_id
 
     async def test_update_state(self, acd_init):
+        """Updates the portal conversation state"""
+
         # Create a puppet in database
         query = "INSERT INTO puppet (custom_mxid) values ($1) RETURNING pk"
         await Portal.db.execute(query, "@acd1:example.com")
 
-        room_id: RoomID = "!qVKwlyUXOCrBfZJOdh:exmple.com"
+        room_id: RoomID = "!qVKwlyUXOCrBfZJOdh:example.com"
         portal: Portal = await Portal.get_by_room_id(room_id=room_id, fk_puppet=1)
 
         await portal.update_state(PortalState.ENQUEUED)
@@ -52,6 +61,8 @@ class TestPortal:
     async def test_get_current_agent(
         self, customer: User, agent_user: User, supervisor: User, mocker: MockerFixture
     ):
+        """Returns the agent who is currently assigned to the portal"""
+
         mocker.patch.object(
             MatrixRoom,
             "get_joined_users",
@@ -62,7 +73,7 @@ class TestPortal:
         query = "INSERT INTO puppet (custom_mxid) values ($1) RETURNING pk"
         await Portal.db.execute(query, "@acd1:example.com")
 
-        room_id: RoomID = "!qVKwlyUXOCrBfZJOdh:exmple.com"
+        room_id: RoomID = "!qVKwlyUXOCrBfZJOdh:example.com"
         portal: Portal = await Portal.get_by_room_id(room_id=room_id, fk_puppet=1)
 
         agent: User = await portal.get_current_agent()
@@ -80,6 +91,8 @@ class TestPortal:
         queue: Queue,
         queue_membership: QueueMembership,
     ):
+        """Checks if there is any online agent in the portal"""
+
         mocker.patch.object(
             MatrixRoom,
             "get_joined_users",
@@ -90,7 +103,7 @@ class TestPortal:
         query = "INSERT INTO puppet (custom_mxid) values ($1) RETURNING pk"
         await Portal.db.execute(query, "@acd1:example.com")
 
-        room_id: RoomID = "!qVKwlyUXOCrBfZJOdh:exmple.com"
+        room_id: RoomID = "!qVKwlyUXOCrBfZJOdh:example.com"
         portal: Portal = await Portal.get_by_room_id(room_id=room_id, fk_puppet=1)
 
         args = ["login", agent_user.mxid]
@@ -123,6 +136,8 @@ class TestPortal:
         queue: Queue,
         queue_membership: QueueMembership,
     ):
+        """Checks if there is no online agent in the portal"""
+
         mocker.patch.object(
             MatrixRoom,
             "get_joined_users",
@@ -133,7 +148,7 @@ class TestPortal:
         query = "INSERT INTO puppet (custom_mxid) values ($1) RETURNING pk"
         await Portal.db.execute(query, "@acd1:example.com")
 
-        room_id: RoomID = "!qVKwlyUXOCrBfZJOdh:exmple.com"
+        room_id: RoomID = "!qVKwlyUXOCrBfZJOdh:example.com"
         portal: Portal = await Portal.get_by_room_id(room_id=room_id, fk_puppet=1)
 
         args = ["logout", agent_user.mxid]
