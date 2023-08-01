1ac8f72be4274b2a66ca3c6122912bb1794e0c81
jzapataikono <jzapata@ikono.com.co>
2023-08-01 16:51:20 -0500

    style: black format

--- acd_appservice/version.py
+++ acd_appservice/version.py
@@ -1,5 +1,7 @@
 # Generated from setup.py
-git_tag = 'v0.2.7.4+dev'
-git_revision = '6c6c8aa4'
-version = '0.2.7.4+dev.6c6c8aa4'
-linkified_version = 'v0.2.7.4+dev.[6c6c8aa4](https://gitlab.com/iKono/acd-appservice/-/commit/6c6c8aa4)'
+git_tag = "v0.2.7.4+dev"
+git_revision = "16d4629e"
+version = "0.2.7.4+dev.16d4629e"
+linkified_version = (
+    "v0.2.7.4+dev.[16d4629e](https://gitlab.com/iKono/acd-appservice/-/commit/16d4629e)"
+)
