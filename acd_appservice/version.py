84166855de6de2f272472ec5286308e910037898
egalvis <egalvis@ikono.com.co>
2023-08-11 17:01:42 -0500

    chore(config): :wrench: Updated meta user prefix

--- acd_appservice/version.py
+++ acd_appservice/version.py
@@ -1,5 +1,5 @@
 # Generated from setup.py
 git_tag = "v0.2.7.4"
-git_revision = "c3dd44cd"
-version = "0.2.7.4+dev.c3dd44cd"
-version_link = "v0.2.7.4+dev.[c3dd44cd](https://gitlab.com/iKono/acd-appservice/-/commit/c3dd44cd)"
+git_revision = "a8b244c8"
+version = "0.2.7.4+dev.a8b244c8"
+version_link = "v0.2.7.4+dev.[a8b244c8](https://gitlab.com/iKono/acd-appservice/-/commit/a8b244c8)"
